bb66d8d62a1b6546f8ff22263849f1cc57c5f894
Spikerman <spikechenhao@163.com>
2016-02-06 18:00:11 +0800

    initial commit

---
+++ src/AppData.java
@@ -0,0 +1,45 @@
+/**
+ * Created by chenhao on 2/5/16.
+ */
+public class AppData {
+    public String name;
+    public String id;
+    public int rank;
+    public double averageUserRating;
+    public double averageUserRatingForCurrentVersion;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public AppData(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public AppData(String id){
+        this.id=id;
+    }
+
+
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
